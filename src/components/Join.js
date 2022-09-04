4c7aa49455e02479135aa7bc791527c40788335c
moses-codes <moses.ilagan@gmail.com>
2022-09-04 15:36:00 -0700

    added form embed

--- src/components/Join.js
+++ src/components/Join.js
@@ -1,13 +1,12 @@
 import React from "react"
 export default function Hero() {
-    return(
+    return (
         <main>
             <h2>Will you join the campaign</h2>
-            <a>Submit</a>
-            <a>Donate</a>
+            <script src="https://www.cognitoforms.com/f/seamless.js" data-key="CQO6e9E-sEmtGc0QTWx8aQ" data-form="1"></script>
 
             <form>
-                
+
             </form>
         </main>
     )
