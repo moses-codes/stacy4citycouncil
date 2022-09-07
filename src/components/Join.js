e155739618372bd56c3bab2f010817f93af3b1be
moses-codes <moses.ilagan@gmail.com>
2022-09-07 14:02:05 -0700

    fixed mobile images

--- src/components/Join.js
+++ src/components/Join.js
@@ -13,7 +13,7 @@ export default function Join() {
     return (
         <footer>
             <div className="flex text-center stacy-win xs:block xl:flex py-20" id='join'>
-                <div className="h-full lg:w-5/12 xs:w-full xs:px-10 xs:mx-auto">
+                <div className="h-full lg:w-5/12 xs:w-auto xs:h-1/4 xs:px-10 xs:mx-auto">
                     <img className='h-full w-auto rounded-xl' src={stacyImg} alt='Stacy poses at a construction site' />
                 </div>
                 <div className="campaign-form bg-white lg:px-10 lg:w-7/12 rounded-xl xs:mx-auto xs:w-full xs:px-2">
