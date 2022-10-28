fd9ac9da0ca8502a34fa24dc26a9c893aeeaf352
moses-codes <moses.ilagan@gmail.com>
2022-10-28 12:41:04 -0700

    changed copy

--- src/components/Status.js
+++ src/components/Status.js
@@ -9,7 +9,7 @@ export default function Status() {
             <div className='p-10'>
                 <h2 className='text-5xl font-semibold mb-10'>October 30, 12:00pm</h2>
                 <h2 className='text-4xl mb-10'>Anita Shower's radio show</h2>
-                <p className='text-lg lg:text-xl mb-10'>Stacy will be Anita Shower's Guest on The Pismo Beach Today Show, which airs on <span className='font-semibold'>KVEC Radio 920 AM/96.5 FM radio Sunday, October 30th, 2022 at noon</span>. Tune in! You can also view a recording of my livestream down below.</p>
+                <p className='text-lg lg:text-xl mb-10'>Stacy will be Anita Shower's Guest on The Pismo Beach Today Show, which airs on <span className='font-semibold'>KVEC Radio 920 AM/96.5 FM radio Sunday, October 30th, 2022 at noon</span>. Tune in! In the meantime, you can also view a recording of Stacy's livestream from October 27 down below.</p>
                 <a className="text-white bg-red-700 py-2 px-10
                     rounded-xl mx-auto xs:text-md text-center"
                     target="_blank" rel="noopener noreferrer"
