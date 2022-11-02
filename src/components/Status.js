3e4ccca374ba603685fe9c1e10fc2a142531cd3d
moses-codes <moses.ilagan@gmail.com>
2022-11-01 21:46:01 -0700

    fixed colors

--- src/components/Status.js
+++ src/components/Status.js
@@ -4,12 +4,12 @@ export default function Status() {
     return (<div className="flex flex-col w-full lg:flex-row mt-0 bg-white">
 
 
-        <div className="card lg:card-side bg-base-100 shadow-xl mx-10 bg-white text-black">
+        <div className="card lg:card-side bg-base-100 shadow-xl mx-10 bg-white text-black mt-10">
             <img className='lg:w-2/4 w-full' src={stacy}></img>
-            <div className='p-10'>
-                <h2 className='text-5xl font-semibold mb-10'>October 30, 12:00pm</h2>
-                <h2 className='text-4xl mb-10'>Anita Shower's radio show</h2>
-                <p className='text-lg lg:text-xl mb-10'>Stacy will be Anita Shower's Guest on The Pismo Beach Today Show, which airs on <span className='font-semibold'>KVEC Radio 920 AM/96.5 FM radio Sunday, October 30th, 2022 at noon</span>. Tune in! In the meantime, you can also view a recording of Stacy's livestream from October 27 down below.</p>
+            <div className='p-10 bg-white'>
+                <h2 className='text-5xl font-semibold mb-10'>Election day is November 8th!</h2>
+                <h2 className='text-4xl mb-10'>Get out and vote.</h2>
+                <p className='text-lg lg:text-xl mb-10'>Get those ballots in. ou can also view a recording of Stacy's livestream from October 27 down below.</p>
                 <a className="text-white bg-red-700 py-2 px-10
                     rounded-xl mx-auto xs:text-md text-center"
                     target="_blank" rel="noopener noreferrer"
