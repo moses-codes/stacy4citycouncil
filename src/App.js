65c960013290ec4d30f3e4fda9ab037edc74596b
moses-codes <moses.ilagan@gmail.com>
2023-01-18 17:54:02 -0800

    takedown message

--- src/App.js
+++ src/App.js
@@ -1,63 +1,13 @@
 import './App.css';
-
-
-import About from './components/About'
-import Banner from './components/Banner'
-import BannerTwo from './components/BannerTwo'
-import Endorsements from './components/Endorsements'
-import Header from './components/Header'
 import Hero from './components/Hero'
-import Positions from './components/Positions'
-import Join from './components/Join'
-import Footer from './components/Footer'
-import FadeInSection from './components/FadeInSection';
-import Updates from './components/Updates'
-import Status from "./components/Status"
-
-import Events from './components/Events'
-
-import Content from './data/content'
 import { BrowserRouter } from 'react-router-dom';
 
 function App() {
 
   return (
-    <BrowserRouter>
-      <div className="w-screen relative bg-white">
-        <div className='header-hero'>
-          <Header className='sticky top-0' />
-          <Hero />
-        </div>
-
-        <Status></Status>
-
-        <Positions
-          id="positions"
-          props={Content}
-        />
-        <FadeInSection>
-          <Banner />
-        </FadeInSection>
-        <About
-          content={Content.about}
-        />
-        <FadeInSection>
-          <Endorsements
-            props={Content}
-          />
-        </FadeInSection>
-
-        <FadeInSection>
-          <BannerTwo />
-        </FadeInSection>
-
-        <FadeInSection>
-          <Join />
-        </FadeInSection>
-
-        <Footer />
-      </div>
-    </BrowserRouter>
+    <>
+      <Hero />
+    </>
   );
 }
 
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,36 +1,21 @@
 import { React, useState } from "react"
-import { NavHashLink as Link } from "react-router-hash-link";
+
+import stacyImg from "../images/stacy-PierPlaza.jpg"
+
 export default function Hero() {
 
     return (
-        <main id='hero' className={"w-full min-h-screen max-h-auto pb-10 block m-0 relative"}>
-            <div className={"xl:pt-36 xl:pl-36 pt-32"}>
+        <main id='hero' className={" w-full min-h-screen max-h-auto pb-10 block m-0 relative bg-white"}>
+            <div className={"xl:pt-96 xl:pl-36 pt-32"}>
                 <h1 className="xs:text-5xl md:text-8xl 
                 md:mb-5 xl:text-left xs:w-auto xs:text-center
-                xs:text-white xs:mb-2">Stacy Inman won!</h1>
+                xs:text-white xs:mb-2">Stacy Inman</h1>
                 <h3
                     className="xs:mb-0 md:md-36 md:text-black
                     xl:text-left xs:text-center md:text-3xl  
                     xs:text-lg xs:text-white"
-                >Pismo Beach City Council Member Elect</h3>
+                >Pismo Beach City Council Member</h3>
 
-            </div>
-            <a className="block w-64 text-2xl xl:my-4 text-white bg-red-700 py-2 px-5 rounded-xl mx-auto xl:ml-36 lg:mt-16 mt:5 text-center"
-                target="_blank" rel="noopener noreferrer"
-                href='https://donorbox.org/stacy-inman-for-city-council'
-
-            >Donate &gt;</a>
-            <div className="w-full bg-blue-900 bg-opacity-80 absolute bottom-0 xl:h-auto block lg:flex h-42">
-                <div className="p-2 text-white lg:w-2/4 w-full leading-normal text-center lg:text-left">
-                    <h2 className="text-sm lg:text-4xl lg:mt-5 px-5">Stacy is officially endorsed by the <span className='italic'>San Luis Obispo Tribune!</span></h2>
-                </div>
-
-                <div className='p-2 text-white  lg:w-2/4 w-full text-center'>
-                    <p className="font-semibold lg:text-xl text-sm">"Inman is a highly qualified candidate who would be a breath of fresh air on the City Council."</p>
-                    <Link
-                        smooth to='#endorsements'
-                        className="block w-72 text-lg xl:my-4 text-white bg-red-700 py-2 px-10 rounded-xl mx-auto mt-5 lg:16 text-center">Read full quote below</Link>
-                </div>
             </div>
 
 
